1399c9398b2cf5c07ee3b8d6ffd5dd03167a9eb2
KendiAstro23 <litalakendy975@gmail.com>
2023-11-01 09:51:57 -0500

    multiply two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,66 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _atoi - function converts string to integer
+ * @s: string to be converted
+ * Return: The converted integer
+ */
+int _atoi(char *s)
+{
+	int x, y, z, n, len, digit;
+
+	x = 0;
+	y = 0;
+	z = 0;
+	n = 0;
+	len = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+	while (x < len && n == 0)
+	{
+		if (s[x] == '-')
+			++y;
+		if (s[x] >= '0' && s[x] <= '9')
+		{
+			digit = s[x] - '0';
+			if (y % 2)
+				digit = -digit;
+			z = z * 10 + digit;
+			n = 1;
+
+			if (s[x + 1] < '0' || s[x + 1] > '9')
+				break;
+			n = 0;
+		}
+		x++;
+	}
+	if (n == 0)
+		return (0);
+	return (z);
+}
+
+
+/**
+ * main - prints the result of the multiplication
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+		printf("Error.\n");
+	return (1);
+	{
+		int n1 = atoi(argv[1]);
+		int n2 = atoi(argv[2]);
+		int mult = n1 * n2;
+
+		printf("%d\n", mult);
+		return (0);
+	}
+}
