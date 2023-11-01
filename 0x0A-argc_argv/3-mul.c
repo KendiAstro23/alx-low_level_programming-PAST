6b629c936ccc4f93fb78ee6c574d8d5d593beb4b
KendiAstro23 <litalakendy975@gmail.com>
2023-11-01 10:46:50 -0500

    multiply two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,48 +3,6 @@
 #include <stdlib.h>
 
 /**
- * _atoi - function converts string to integer
- * @s: string to be converted
- * Return: The converted integer
- */
-int _atoi(char *s)
-{
-	int x, y, z, n, len, digit;
-
-	x = 0;
-	y = 0;
-	z = 0;
-	n = 0;
-	len = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-	while (x < len && n == 0)
-	{
-		if (s[x] == '-')
-			++y;
-		if (s[x] >= '0' && s[x] <= '9')
-		{
-			digit = s[x] - '0';
-			if (y % 2)
-				digit = -digit;
-			z = z * 10 + digit;
-			n = 1;
-
-			if (s[x + 1] < '0' || s[x + 1] > '9')
-				break;
-			n = 0;
-		}
-		x++;
-	}
-	if (n == 0)
-		return (0);
-	return (z);
-}
-
-
-/**
  * main - prints the result of the multiplication
  * @argc: argument count
  * @argv: argument vector
@@ -52,15 +10,13 @@ int _atoi(char *s)
  */
 int main(int argc, char *argv[])
 {
-	int result, n1 = 0, n2 = 0;
+	int n1 = 0, n2 = 0;
 
 	if (argc == 3)
 	{
-		n1 = _atoi(argv[1]);
-		n2 = _atoi(argv[2]);
-		result = n1 * n2;
-
-		printf("%d\n", result);
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%d\n", n1 * n2);
 	}
 	else
 	{
