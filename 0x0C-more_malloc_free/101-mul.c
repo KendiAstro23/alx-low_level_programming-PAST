a4c7b11b509f217496ad19a46826824334fdd13a
KendiAstro23 <litalakendy975@gmail.com>
2023-11-02 17:57:13 -0500

    multiply

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,9 +3,10 @@
 #include <stddef.h>
 
 /**
- * _print - moves a string from one place to another
- * @str: string to be moved
+ * _print - moves a string one place to the left and prints the string
+ * @str: string to move
  * @l: size of string
+ *
  * Return: void
  */
 void _print(char *str, int l)
@@ -27,12 +28,13 @@ void _print(char *str, int l)
 }
 
 /**
- * mul - multiplies a char with a string
+ * mul - multiplies a char with a string and places the answer into dest
  * @n: char to multiply
  * @num: string to multiply
  * @num_index: last non NULL index of num
  * @dest: destination of multiplication
  * @dest_index: highest index to start addition
+ *
  * Return: pointer to dest, or NULL on failure
  */
 char *mul(char n, char *num, int num_index, char *dest, int dest_index)
@@ -62,8 +64,9 @@ char *mul(char n, char *num, int num_index, char *dest, int dest_index)
 }
 
 /**
- * check_for_digits - checks the arguments
+ * check_for_digits - checks the arguments to ensure they are digits
  * @av: pointer to arguments
+ *
  * Return: 0 if digits, 1 if not
  */
 int check_for_digits(char **av)
