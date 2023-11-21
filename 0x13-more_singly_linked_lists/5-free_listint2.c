03bea0070abf305eb425928f4a73f692734abc88
KendiAstro23 <litalakendy975@gmail.com>
2023-11-21 15:36:15 -0600

    free list head NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current;
 
-	while (*head != NULL)
+	while (*head)
 	{
 		current = *head;
 		*head = (*head)->next;
 		free(current);
+		*head = current;
 	}
-	*head = NULL;
+	if (*head == NULL)
+		return;
 }
