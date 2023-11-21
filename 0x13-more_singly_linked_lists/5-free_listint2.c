baae832b7a4d52d62f7cc13e71738962e8381ef6
KendiAstro23 <litalakendy975@gmail.com>
2023-11-21 15:41:26 -0600

    free list head NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * free_listint2 - frees list
  * @head: beginning
@@ -9,13 +10,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current;
 
-	while (*head)
+	while (*head != NULL)
 	{
 		current = *head;
 		*head = (*head)->next;
 		free(current);
-		*head = current;
 	}
-	if (*head == NULL)
-		return;
+	*head = NULL;
 }
