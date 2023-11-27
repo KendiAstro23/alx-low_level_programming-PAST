7b17b7e60ea0e3b528953e8b29657fc9e0691482
KendiAstro23 <litalakendy975@gmail.com>
2023-11-27 14:56:53 -0600

    copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include "main.h"
 /**
- * error_file - checks if files can be opened.
- * @file_from: file_from.
- * @file_to: file_to.
- * @argv: arguments vector.
- * Return: no return.
+ * error_file - looks for errors in a file
+ * @file_from: previous destination of file
+ * @file_to: last destination
+ * for a file
+ * @argv: pointer of argument vector
+ * Return: file content
  */
 void error_file(int file_from, int file_to, char *argv[])
 {
@@ -22,45 +24,44 @@ void error_file(int file_from, int file_to, char *argv[])
 }
 
 /**
- * main - check the code for Holberton School students.
- * @argc: number of arguments.
- * @argv: arguments vector.
- * Return: Always 0.
+ * main - prints code
+ * @argv: the argument vector
+ * @argc: the argument count
+ * Return: 0 for success
  */
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
-	ssize_t nchars, nwr;
-	char buf[1024];
+	ssize_t nstr, bytewrite;
+	char buffer[1024];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
-
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
 
-	nchars = 1024;
-	while (nchars == 1024)
+	nstr = 1024;
+	while (nstr == 1024)
 	{
-		nchars = read(file_from, buf, 1024);
-		if (nchars == -1)
+		nstr = read(file_from, buffer, 1024);
+		if (nstr == -1)
 			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
-		if (nwr == -1)
+		bytewrite = write(file_to, buffer, nstr);
+		{
+		if (bytewrite == -1)
 			error_file(0, -1, argv);
+		}
 	}
-
 	err_close = close(file_from);
 	if (err_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-
 	err_close = close(file_to);
 	if (err_close == -1)
 	{
